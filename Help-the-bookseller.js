c0691c4170f48b9f8a9a0b281bb9bf7a12e2d083
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-30 18:41:59 -0700

    Update Help-the-bookseller.js

--- Help-the-bookseller.js
+++ Help-the-bookseller.js
@@ -38,3 +38,16 @@ Notes:
 
 // Solution
 
+function stockList(listOfArt, listOfCat) {
+  var qs = {};
+  if (!listOfArt.length) return '';
+
+  listOfArt.forEach(function(art) {
+    var cat = art[0];
+    qs[cat] = (qs[cat] | 0) + +art.split(' ')[1];
+  });
+
+  return listOfCat.map(function(c) {
+    return '(' + c + ' : ' + (qs[c] | 0) + ')';  
+  }).join(' - ');  
+}
