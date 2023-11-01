baf2420308ee91f3c8c4d3f7d282ea74e6f7f9c7
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-31 18:56:31 -0700

    Update Help-the-bookseller.js

--- Help-the-bookseller.js
+++ Help-the-bookseller.js
@@ -19,8 +19,7 @@ M = ["A", "B", "C", "W"] or ...
 and your task is to find all the books of L with codes belonging to each 
 category of M and to sum their quantity according to each category.
 
-For the lists L and M of example you have to return the string 
-(in Haskell/Clojure/Racket/Prolog a list of pairs):
+For the lists L and M of example you have to return the string:
 
 (A : 20) - (B : 114) - (C : 50) - (W : 0)
 
@@ -29,10 +28,10 @@ where A, B, C, W are the categories,
 114 the sum corresponding to "BKWRK" and "BTSQZ", 
 50 corresponding to "CDXEF" and 0 to category 'W' since there are no code beginning with W.
 
-If L or M are empty return string is "" (Clojure/Racket/Prolog should return an empty array/list instead).
+If L or M are empty return string is "".
+
 Notes:
   In the result codes and their values are in the same order as in M.
-  See "Samples Tests" for the return.
 */
 
 
